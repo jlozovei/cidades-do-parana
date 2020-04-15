edc18e2a55d74a14a17b2818cda752d6f75168b6
jlozovei <jlozovei@gmail.com>
2020-04-15 10:11:59 -0300

    add routes
    - / and /about

---
+++ src/routes/About.svelte
@@ -0,0 +1,14 @@
+<h1>Sobre o jogo</h1>
+
+<p><strong>Cidades do Paraná</strong> é um jogo educativo, feito para você testar seus conhecimentos geográficos sobre o estado do Paraná. É indicado para todas as idades.</p>
+
+<p>O objetivo é fácil: acertar todos, ou o maior número de municípios. Porém, dentro de 5 minutos. Desafie seus amigos e familiares!</p>
+
+
+<h2>Créditos</h2>
+
+<ul>
+  <li>Esse site é feito por <a href="https://jlozovei.dev" target="_blank" rel="noopener noreferrer">Julio Lozovei</a></li>
+  <li>A inspiração veio do jogo <a href="https://cidadesdo.es/" target="_blank" rel="noopener noreferrer">Cidades do Espírito Santo</a>, feito pelo meu amigo <a href="https://vitorbortolotti.github.io/" target="_blank" rel="noopener noreferrer">Vitor Bortolotti</a>.</li>
+  <li>O mapa do Paraná utilizado está na <a href="https://pt.wikipedia.org/wiki/Ficheiro:Brazil_Parana_location_map.svg" target="_blank" rel="noopener noreferrer">Wikipedia</a></li>
+</ul>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { game } from '../store/game.js';
+  import Game from "../components/Game.svelte";
+  import Map from "../components/Map.svelte";
+
+  let hasGameStarted;
+
+  const subscribe = game.subscribe(value => {
+    hasGameStarted = value;
+  });
+</script>
+
+<h1 class="page__title">Cidades do Paraná</h1>
+
+<div class="page__map">
+  <Map />
+</div>
+
+{#if hasGameStarted}
+  <Game />
+{:else}
+  <div class="page__text">
+    <p>O Paraná tem 399 municípios; você conhece todos eles? Teste seu conhecimento sobre o nosso estado nesse jogo.</p>
+    <p>Como jogar:</p>
+
+    <ul>
+      <li>Escreva o nome da cidade no campo indicado - se você acertar, a localização será mostrada no mapa;</li>
+      <li>Não precisa utilizar acentos gráficos nos nomes dos municípios;</li>
+      <li>Você tem 5 minutos para jogar;</li>
+      <li>No final do jogo, você poderá ver a sua pontuação e tentar novamente!</li>
+    </ul>
+
+    <p>Preparado?</p>
+
+    <button on:click={game.start}>Começar</button>
+  </div>
+{/if}
